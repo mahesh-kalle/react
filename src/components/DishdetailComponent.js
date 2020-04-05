188b37041c685eed386c60a055bd42751b337a5c
Mahesh Kalle <kallemahesh123@gmail.com>
2020-04-04 18:28:53 -0600

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments({comments}) {
         if (comments == null) {
@@ -20,6 +21,7 @@ import { Card, CardImg, CardText, CardBody,
             )
         })
         return (
+            
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
@@ -52,12 +54,27 @@ import { Card, CardImg, CardText, CardBody,
     const DishDetail = (props) => {
 
         if (props.dish != null) {
-            return( 
-                <div className='container'>
-                    <div className="row">
-                     <RenderDish dish={props.dish} />
-                     <RenderComments comments={props.dish.comments} />
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
                 </div>
             );
         } else {
