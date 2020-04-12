a1713a049527dba5a8915a7bc7d6eea3ae3768df
Mahesh Kalle <kallemahesh123@gmail.com>
2020-04-11 21:29:58 -0600

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render () {
@@ -99,7 +99,7 @@ class CommentForm extends Component {
     };
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
         var displayComments = comments.map((comment) => {
             return (
@@ -125,7 +125,7 @@ function RenderComments({comments, addComment, dishId}) {
             <div>
                 <h4>Comments</h4>
                 {displayComments}
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     } else {
@@ -194,7 +194,7 @@ function RenderComments({comments, addComment, dishId}) {
                     <div className="col-12 col-md-5 m-1">
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />
                     </div>
